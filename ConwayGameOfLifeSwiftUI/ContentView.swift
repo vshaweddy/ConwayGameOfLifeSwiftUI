e34764083dc99454bbaf2a3a5fcece5b4fc589a2
Vici Shaweddy <vshaweddy@gmail.com>
2020-08-22 11:43:50 -0400

    initial commit

---
+++ ConwayGameOfLifeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConwayGameOfLifeSwiftUI
+//
+//  Created by Vici Shaweddy on 8/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Vici!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
