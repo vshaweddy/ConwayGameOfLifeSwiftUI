a1453971d80b830e92a377af17f825e9919927af
Vici Shaweddy <vshaweddy@gmail.com>
2020-08-22 21:09:14 -0400

    set up the gridview

--- ConwayGameOfLifeSwiftUI/ContentView.swift
+++ ConwayGameOfLifeSwiftUI/ContentView.swift
@@ -7,10 +7,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Board {
+    let rows: Int
+    let cols: Int
+    let allCells: [Cell]
+    
+    init(rows: Int, cols: Int) {
+        self.rows = rows
+        self.cols = cols
+        
+        var allCells = [Cell]()
+        for row in 0..<rows {
+            for col in 0..<cols {
+                allCells.append(Cell(row: row, col: col))
+            }
+        }
+        self.allCells = allCells
+    }
+}
+
+extension Board {
+    var columns: [GridItem] {
+        [GridItem](repeating: GridItem(.fixed(20)), count: cols)
+    }
+}
+
+final class Cell: Identifiable {
+    enum Status {
+        case alive, dead
+    }
+    
+    // The row of the cell
+    let row: Int
+    
+    // The col of the cell
+    let col: Int
+    
+    // The status of the cell
+    var status: Status
+    
+    // creating id for each cell
+    var id: (Int, Int) {
+        (row, col)
+    }
+    
+    init(row: Int, col: Int) {
+        self.row = row
+        self.col = col
+        self.status = [Cell.Status.dead, .alive].randomElement() ?? .dead
+    }
+}
+
+
+
+struct CellView: View {
+    let status: Cell.Status
+    
     var body: some View {
-        Text("Hello, Vici!")
-            .padding()
+        Text(status == .alive ? "🦠" : "✨")
+    }
+}
+
+struct ContentView: View {
+    private let board = Board(rows: 10, cols: 10)
+
+    
+    var body: some View {
+        LazyVGrid(columns: board.columns) {
+            ForEach(board.allCells, id: \.id) { cell in
+                CellView(status: cell.status)
+            }
+        }
     }
 }
 
